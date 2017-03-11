1306327920d82f12b1b414edf84d59cfcc28c942
JD Hiudt <email": null>
2017-03-11 11:46:38 -0600

    added info for attendence table

--- db/migrate/20170311174051_create_attendences.rb
+++ db/migrate/20170311174051_create_attendences.rb
@@ -1,8 +1,10 @@
 class CreateAttendences < ActiveRecord::Migration[5.0]
   def change
     create_table :attendences do |t|
+      t.integer :attendee_id, null: false
+      t.integer :concert_id, null: false
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
