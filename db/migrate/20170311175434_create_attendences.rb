e9ca8cd0cec2fdd0e853576c3379c5bdb51b0999
JD Hiudt <email": null>
2017-03-11 12:02:12 -0600

    populate attendence table

--- db/migrate/20170311175434_create_attendences.rb
+++ db/migrate/20170311175434_create_attendences.rb
@@ -1,8 +1,9 @@
 class CreateAttendences < ActiveRecord::Migration[5.0]
   def change
     create_table :attendences do |t|
-
-      t.timestamps
+      t.integer :attendee_id, null: false
+      t.integer :concert_id, null: false
+      t.timestamps(null: false)
     end
   end
 end
