8efbe6a3ded3815f6755f011a801c2b5150a69ef
JD Hiudt <email": null>
2017-03-11 11:58:57 -0600

    populate user table

--- db/migrate/20170311175416_create_users.rb
+++ db/migrate/20170311175416_create_users.rb
@@ -1,8 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
-      t.timestamps
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :hashed_password, null: false
+      t.timestamps(null: false)
     end
   end
 end
