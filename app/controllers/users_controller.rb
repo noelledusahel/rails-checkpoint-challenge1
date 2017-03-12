8a6278806a3252cfc95c696323a3624dafe84fe7
noelledusahel <barberossa@gmail.com>
2017-03-12 00:42:20 -0600

    add login in log out register methods to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,33 @@
 class UsersController < ApplicationController
 
+	def new
+		@user = User.new
+	end 
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			session[:user] = @user.id
+			redirect_to concerts_path
+		else
+			@errors = @user.errors.full_messages
+			render 'new'
+		end 
+	end 
+
+	def destroy
+		@user = User.find_by(id: session[:user])
+		session[:user] = nil
+		redirect_to concerts_path
+	end 
+
+
+
+	private 
+
+	def user_params 
+		params.require(:user).permit(:first_name, :last_name, :email, :password)
+	end
+
 end 
--- app/models/concert.rb
+++ app/models/concert.rb
@@ -2,4 +2,6 @@ class Concert < ApplicationRecord
 
   has_many :attendances
   has_many :patrons, through: :attendances, source: :user
+
+  validates :band, :venue, :date, :start_time, presence: true
 end
