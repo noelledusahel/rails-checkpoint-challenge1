04bd4725b65a75f9b0cbd2a94c5951479f392d3c
noelledusahel <barberossa@gmail.com>
2017-03-11 23:15:29 -0600

    add concert controller methods and pass tests

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -2,7 +2,32 @@ class ConcertsController < ApplicationController
 
 	def index
 		@concerts = Concert.all
-
 	end
+
+	def show
+		@concert = Concert.find_by(id: params[:id])
+
+	end 
+
+	def new 
+		@concert = Concert.new
+	end 
+
+	def create
+		@concert = Concert.new(concert_params)
+
+		if @concert.save
+			redirect_to concert_path(@concert)
+		else
+			render 'new'
+		end 
+	end 
+
+	private
+
+	def concert_params
+		params.require(:concert).permit(:band, :venue, :date, :start_time)
+	end 
+
 
 end 
